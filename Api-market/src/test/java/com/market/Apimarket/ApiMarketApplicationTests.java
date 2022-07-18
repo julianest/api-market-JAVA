520815dd31d2e612f28d49395ef4e15abda9799e
julianest <julianest12@gmail.com>
2022-07-18 15:03:39 -0500

    first commit

---
+++ Api-market/src/test/java/com/market/Apimarket/ApiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.Apimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
